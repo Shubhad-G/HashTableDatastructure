9780e040d64baabe588e4e83859df1ec28855a42
Shubhad Gonal <shubhadgonal19@gmail.com>
2022-12-04 12:27:13 +0530

    Remove avoidable word from the phrase “Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations”

--- HashTableDataStructure/MyMapNode.cs
+++ HashTableDataStructure/MyMapNode.cs
@@ -100,19 +100,6 @@ namespace HashTableDataStructure
                     }
             }
         }
-        public void CountNumbOfOccurence(string paragraph)
-        {
-            MyMapNode<string, int> hashTabe = new MyMapNode<string, int>(6);
-            string[] words = paragraph.Split(' ');
-            foreach (string word in words)
-            {
-                if (hashTabe.Exists(word.ToLower()))
-                    hashTabe.Add(word.ToLower(), hashTabe.Get(word.ToLower()) + 1);
-                else
-                    hashTabe.Add(word.ToLower(), 1); //to,1 
-            }
-            Console.WriteLine("Words and their frequency of occurrences ");
-            hashTabe.Display();
-        }
+       
     }
 }
--- HashTableDataStructure/Program.cs
+++ HashTableDataStructure/Program.cs
@@ -9,19 +9,31 @@ namespace HashTableDataStructure
 {
     internal class Program
     {
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hash table program");
-          
             MyMapNode<string, int> hashTabe = new MyMapNode<string, int>(6);
-            string paragraph = "Paranoids are not paranoid " +
-                        "because they are paranoid but " +
-                        "because they keep putting themselves " +
-                        "deliberately into paranoid avoidable " +
-                        "situations";
-             hashTabe.CountNumbOfOccurence(paragraph);
-           
-            
+            string paragraph2 = "Paranoids are not paranoid " +
+                "because they are paranoid but " +
+                "because they keep putting themselves " +
+                "deliberately into paranoid avoidable " +
+                "situations";
+            string[] words = paragraph2.Split(' ');
+            foreach (string word in words)
+            {
+                if (hashTabe.Exists(word.ToLower()))
+                    hashTabe.Add(word.ToLower(), hashTabe.Get(word.ToLower()) + 1);
+                else
+                    hashTabe.Add(word.ToLower(), 1); //to,1 
+            }
+            Console.WriteLine("All words counted .");
+            hashTabe.Display();
+            string s = "avoidable";
+            hashTabe.Remove(s);
+            Console.WriteLine();
+            Console.WriteLine($"A word \"{s}\" has been removed ");
+            hashTabe.Display();
+
             Console.ReadLine();
 
         }
